653a028a1dd3c1bce446dcca23adee5b81f799ba
Dave Banwell <dave@davebanwell.com>
2025-06-12 19:20:12 -0400

    all tests passing

--- server/app.py
+++ server/app.py
@@ -5,6 +5,8 @@ from flask_migrate import Migrate
 
 from models import db, Bakery, BakedGood
 
+import ipdb
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -20,19 +22,25 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakery_lst = [b.to_dict() for b in Bakery.query.all()]
+    return make_response(bakery_lst, 200)
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+    return make_response(bakery.to_dict(), 200)
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    bg_lst = [b.to_dict() for b in BakedGood.query.all()]
+    sorted_bg_lst = sorted(bg_lst, key=lambda item: item["price"], reverse=True)
+    return make_response(sorted_bg_lst, 200)
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    bg_lst = [b.to_dict() for b in BakedGood.query.all()]
+    max_price_bg = max(bg_lst, key=lambda item: item["price"])
+    return make_response(max_price_bg, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
